355f5b560960471be191950dab752cd321cca4b4
alex-smirnoff <128931498+alex-smirnoff@users.noreply.github.com>
2023-12-08 01:34:34 +0200

    Add files via upload

---
+++ js/dataAboutCompany.js
@@ -0,0 +1,60 @@
+let assetsUKR = {
+	"ТОВ «Метінвест Холдинг»": ["м. Київ,\u200B Лабораторний провулок, 12"],
+	"ТОВ «Метінвест-СМЦ»": ["м. Київ,\u200B вул. Князів Острозьких, 8",
+							"м. Львів,\u200B вул. Шевченка, 317-С",
+							"м. Дніпро,\u200B вул. Солончакова, 101-А",
+							"м. Бровари, вул. Богдана\u200B Хмельницького, 1",
+							"с. Святопетрівське,\u200B вул. Київська, 27",
+							"м. Вінниця,\u200B вул. Грибоєдова 1-А",
+							"м. Хмельницький,\u200B вул. Курчатова, 118-А",
+							"м. Кременчук,\u200B вул. Ярмаркова 1-Б",
+							"м. Тернопіль,\u200B вул. Поліська, 11",
+							"м. Кривий Ріг,\u200B Широківське шосе, 8",
+							"м. Миколаїв,\u200B вул. Яворницького, 28",
+							"смт. Васищеве,\u200B вул. Промислова, 15-А",
+							"м. Одеса,\u200B Новомосковська дорога, 23/1",
+							"м. Одеса,\u200B Старокиївське шосе, 21",
+							"м. Брошнів-Осада,\u200B вул. 22-го Січня, 28-Б",],
+	"ТОВ «Метінвест Діджитал»": ["м. Київ,\u200B Лабораторний провулок, 12"],
+	"ТОВ «Метінвест-Шиппінг»": ["м. Запоріжжя,\u200B Південне шосе, 72"],
+	"Металургійний комбінат «Запоріжсталь»": ["м. Запоріжжя,\u200B Південне шосе, 72"],
+	"«Каметсталь»": ["м. Кам'янське,\u200B вул. Соборна, 186"]
+}
+let webPagesOfAssetsUKR = {
+    "ТОВ «Метінвест Холдинг»": "metinvestholding.com",
+	"ТОВ «Метінвест-СМЦ»": "metinvest-smc.com",
+	"ТОВ «Метінвест Діджитал»": "metinvest.digital",
+	"ТОВ «Метінвест-Шиппінг»": "metinvestholding.com/ua/shipping",
+	"Металургійний комбінат «Запоріжсталь»": "zaporizhstal.com",
+	"«Каметсталь»": "dkhz.com.ua"
+}
+let assetsENG = {
+	"Metinvest Holding LLC": ["Kyiv,\u200B 12 Laboratorny Lane"],
+	"Metinvest-SMC LLC": ["Kyiv,\u200B 8 Kniaziv Ostrozkych Street",
+						"Lviv,\u200B 317-C Shevchenko Street",
+						"Dnipro,\u200B 101-A Solonchakova Street",
+						"Brovary, 1 Bohdana\u200B Khmelnytskoho Street",
+						"Sviatopetrivske,\u200B 27 Kyivska Street",
+						"Vinnytsia,\u200B 1-A Hryboyedova Street",
+						"Khmelnytsky,\u200B 118-A Kurchatova Street",
+						"Kremenchuk,\u200B 1-B Yarmarkova Street",
+						"Ternopil,\u200B 11 Poliska Street",
+						"Kryvy Rih,\u200B 8 Shyrokivske Highway",
+						"Mykolayiv,\u200B 28 Yavornytskoho Street",
+						"Vasyshcheve,\u200B 15-A Promyslova Street",
+						"Odesa,\u200B 23/1 Novomoskovska Road",
+						"Odesa,\u200B 21 Starokyivske Highway",
+						"Broshniv-Osada,\u200B 28-B 22th Sichnia Street"],
+	"Metinvest Digital LLC": ["Kyiv,\u200B 12 Laboratorny Lane"],
+	"Metinvest Shipping LLC": ["Zaporizhzhia,\u200B 72 Pivdenne Highway"],
+	"Kametstal": ["Kamyanske,\u200B 186 Soborna Street"]
+}
+let webPagesOfAssetsENG = {
+    "Metinvest Holding LLC": "metinvestholding.com",
+	"Metinvest-SMC LLC": "metinvest-smc.com",
+	"Metinvest Digital LLC": "metinvest.digital",
+	"Metinvest Shipping LLC": "metinvestholding.com/en/shipping",
+	"Zaporizhstal Iron and Steel Works": "zaporizhstal.com",
+	"Kametstal": "dkhz.com.ua"
+}
+
